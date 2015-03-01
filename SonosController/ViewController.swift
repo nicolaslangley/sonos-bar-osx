2f84a250cd70a5891480dace2e700b5ca2ea5895
Nicolas Langley <nicolasqlangley@gmail.com>
2015-02-28 20:02:11 -0800

    Initial Commit

---
+++ SonosController/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SonosController
+//
+//  Created by Nicolas Langley on 2/28/15.
+//  Copyright (c) 2015 Nicolas Langley. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
